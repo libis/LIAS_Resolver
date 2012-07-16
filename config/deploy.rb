8440833f69fc201c534d754058c8fa80257150b7
Kris Dekeyser <kris.dekeyser@libis.be>
2012-07-16 10:31:33 +0200

    Fix for deployment

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,6 +8,8 @@ set :application, "LIAS Resolver"
 set :repository,  "git@github.com:Kris-LIBIS/LIAS_Resolver.git"
 
 set :scm, :git
+set :branch, 'master'
+set :stage, 'production'
 
 set :deploy_to, "/opt/libis/LIAS_Resolver"
 set :user, "exlibris"
@@ -26,30 +28,33 @@ namespace :remote do
   task :fw_on do
     run "sudo /etc/init.d/firewall start"
   end
+  
+  task :create_dirs do
+    run "mkdir -p #{shared_path}/log"
+    run "mkdir -p #{shared_path}/pid"
+  end
 
   task :create_symlinks do
-    run "mkdir -fp #{shared_path}/log"
-    run "ln -s #{shared_path}/log #{current_path}/log"
-    run "mkdir -fp #{shared_path}/pid"
-    run "ln -s #{shared_path}/log #{current_path}/pid"
+    run "ln -ns #{shared_path}/log #{current_path}/"
+    run "ln -ns #{shared_path}/pid #{current_path}/"
+    run "ln -ns #{shared_path}/lias_resolver.yml #{current_path}/"
   end
 
   task :stop_server do
-    run "sudo /etc/thin stop"
+    run "sudo /etc/init.d/thin stop"
   end
 
   task :start_server do
-    run "sudo /etc/thin start"
+    run "sudo /etc/init.d/thin start"
   end
 
 end
 
-
-after  "deploy:update", "deploy:cleanup"
+after  "deploy:setup" , "remote:create_dirs"
 
 before "deploy:update_code", "remote:fw_off"
 after  "deploy:update_code", "remote:fw_on"
 
 before "deploy:update", "remote:stop_server"
-after  "deploy:update", "remote:start_server"
+after  "deploy:update", "remote:create_symlinks", "remote:start_server", "deploy:cleanup"
 
