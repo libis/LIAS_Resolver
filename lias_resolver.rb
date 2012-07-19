988a55662081875c0add6df9da1153133b621b8f
Kris Dekeyser <kris.dekeyser@libis.be>
2012-07-19 22:42:24 +0200

    New features: - sorting option for find_pid. - support for user supplied operator for find_pid. - on-line help enabled. - metadata record ids included in find_pid result Fixes: - filter out suppressed and private objects. This should close #1.

--- lias_resolver.rb
+++ lias_resolver.rb
@@ -2,10 +2,12 @@
 
 require 'sinatra'
 require 'sinatra/base'
+#noinspection RubyResolve
 require 'sinatra/config_file'
 require 'builder'
 require 'cgi'
 require 'set'
+require 'haml'
 require './lib/digital_entity_explorer'
 require './lib/digital_entity_manager'
 require './lib/meta_data_manager'
@@ -13,6 +15,7 @@ require './lib/xml_document'
 
 require './lias_resolver_helper'
 
+#noinspection RubyStringKeysInHashInspection,RubyResolve
 class LiasResolver < Sinatra::Base
 
   register Sinatra::ConfigFile
@@ -22,31 +25,52 @@ class LiasResolver < Sinatra::Base
   digital_entity_explorer = DigitalEntityExplorer.new
   connection = nil
 
+  set :static, true
+
   before do
     cache_control :public, :max_age => 36000
   end
 
   get '/find_pid' do
+    key = params['key']
     term = params['search']
     max = params['max_results']
     from = params['from']
     filter_text = params['filter']
+    sort_text = params['sort']
+    operator = params['operator']
 
+puts params[:operator]
+
+    key ||= 'label'
     max = max.to_i | 20
     from = from.to_i
 
-    halt 400, 'Missing \'term\' parameter' if term.nil? or term.empty?
+    options = {}
 
-    filter = {}
     filter_text.split('|').each do |f|
       a = f.split(':')
-      filter[a[0]] = a[1]
+      options[:filter] ||= {}
+      options[:filter][a[0]] = a[1]
     end if filter_text
 
-    result = digital_entity_explorer.search('label', term, from, max, filter)
-puts result[:result]
+    sort_text.split('|').each do |s|
+      if s.downcase == 'random'
+        options[:sort] = %w(dbms_random.value)
+        break
+      end
+      options[:sort] ||= []
+      options[:sort] << s
+    end if sort_text
+
+    if operator
+      options[:operator] = operator
+    end
+
+    result = digital_entity_explorer.search(key, term, from, max, options)
 
     pid_list = result[:pids]
+
     totals = result[:result].xpath('//@total_num_results')
     total = 0
     total = totals.first.content.to_i if totals
@@ -57,16 +81,17 @@ puts result[:result]
 
     headers 'Content-type' => 'text/xml', 'Charset' => 'utf-8'
 
-    builder do |xml|
+    xml = builder do |xml|
 
       xml.instruct! :xml, version: '1.0', encoding: 'utf-8'
 
       attributes = {
-        'search' => "#{CGI::escapeHTML(term)}",
         'total'  => "#{total.to_s}",
         'first'  => "#{from.to_s}",
         'last'   => "#{_last.to_s}"
       }
+
+      attributes['search'] = "#{CGI::escapeHTML(term)}" if term
 
       attributes['next'] = "#{_next.to_s}" if _more > 0
       attributes['more'] = "#{_more.to_s}" if _more > 0
@@ -90,17 +115,20 @@ puts result[:result]
             'view'      => "#{CGI::escapeHTML(v_url)}"
           }
 
-          attributes['children'] = "#{CGI::escapeHTML(c_url)}" if ['COMPLEX', 'METS'].include?(etype)
+          attributes['children'] = "#{CGI::escapeHTML(c_url)}" if %w(COMPLEX METS).include?(etype)
 
           xml.item(attributes) do
 
-            de.xpath('//mds/md[name = \'descriptive\']').each { |md|
+            de.xpath("//xb:digital_entity[pid=$pid]/mds/md[name = 'descriptive']", nil, { :pid => p.to_s }).each { |md|
+
               mid = md.xpath('mid').first.content
+
               attributes = {
                 'mid' => mid,
                 'type' => md.xpath('type').first.content,
                 'url' => settings.this_url + '/get_metadata?mid=' + mid
               }
+
               xml.metadata(attributes)
             }
 
@@ -213,32 +241,32 @@ puts result[:result]
     sql = make_sql lookup_type
     pid_list = run_query sql, pid, usagetype, connection
 
-    unless viewer.nil?
-      extra_params = params
-      extra_params.delete 'usagetype'
-      extra_params.delete 'redirect'
-
-      halt 400, 'Object not found. PID or manifestation does not exist.' unless pid_list.size > 0 
-
-      extra_params['pid'] = pid_list.first
-
-      viewer += "?"
-      extra_params.each do |k,v|
-        viewer += "#{k.to_s}=#{v.to_s}&"
-      end # each extra_params
-      viewer += "custom_att_2=simple_viewer"
-      redirect viewer
-
-    else
+    if viewer.nil?
       headers 'Content-type' => 'text/xml', 'Charset' => 'utf-8'
       builder do |xml|
         xml.instruct! :xml, version: '1.0', encoding: 'utf-8'
-        xml.result('source_pid' => pid,'target_usagetype'  => usagetype.join(',')) do
+        xml.result('source_pid' => pid, 'target_usagetype' => usagetype.join(',')) do
           pid_list.each do |p|
             xml.target_pid p.to_s
           end # pid_list.each
         end # xml.result
       end # builder
+
+    else
+      extra_params = params
+      extra_params.delete 'usagetype'
+      extra_params.delete 'redirect'
+
+      halt 400, 'Object not found. PID or manifestation does not exist.' unless pid_list.size > 0
+
+      extra_params['pid'] = pid_list.first
+
+      viewer += "?"
+      extra_params.each do |k, v|
+        viewer += "#{k.to_s}=#{v.to_s}&"
+      end # each extra_params
+      viewer += "custom_att_2=simple_viewer"
+      redirect viewer
 
     end # unless
 
@@ -289,13 +317,36 @@ puts result[:result]
         result[:result].xpath('//mds/md[name=\'descriptive\']').each { |md|
           xml.metadata(mid: md.xpath('mid').first.content, type: md.xpath('type').first.content) {
             doc = XmlDocument.parse(md.xpath('value').first.content)
-            xml << doc.root.to_xml + "\n"
+            xml << doc.root.to_xml + "\n" if doc and doc.root
           } # xml.metadata
         } # each md
       } # xml.result
     } # builder
   end # get_metadata
 
+  get '/help' do
+    haml :help
+  end
+
+  get '/find_pid.help' do
+    haml :find_pid
+  end
+
+  get '/get_children.help' do
+    haml :get_children
+  end
+
+  get '/get_pid.help' do
+    haml :get_pid
+  end
+
+  get '/get_mid.help' do
+    haml :get_mid
+  end
+
+  get '/get_metadata.help' do
+    haml :get_metadata
+  end
 
   run! if app_file == $0
 
--- lib/digital_entity_explorer.rb
+++ lib/digital_entity_explorer.rb
@@ -6,52 +6,74 @@ class DigitalEntityExplorer < SoapClient
     super "DigitalEntityExplorer"
   end
 
-  def search( tag, term, from, max, filter = nil)
-    query = create_query tag, term, from, max, filter
+  def search(tag, term, from, max, options = {})
+    query = create_query tag, term, from, max, options
     request :digital_entity_search, :general => general.to_s, :query => query.to_s
   end
 
-  def create_query( tag, term, from, max, filter )
+  def create_query(tag, term, from, max, options)
     from += 1 # bug in DTL web service
     query = create_document
     root = create_node('x_queries',
-                       :namespaces => { :node_ns  => 'xb',
-                                        'xb'      => 'http://com/exlibris/digitool/repository/api/xmlbeans'})
+                       :namespaces => {:node_ns => 'xb',
+                                       'xb' => 'http://com/exlibris/digitool/repository/api/xmlbeans'})
     query.root = root
 
-    root << (x_query = create_node('x_query', :attributes => { 'type' => 'hql', 'name' => 'SET1' }))
-    x_query << (x_select = create_node('x_select', :attributes => { 'from_answer' => from.to_s, 'to_answer' => (from + max - 1).to_s }))
+    root << (x_query = create_node('x_query', :attributes => {'type' => 'hql', 'name' => 'SET1'}))
+    x_query << (x_select = create_node('x_select', :attributes => {'from_answer' => from.to_s, 'to_answer' => (from + max - 1).to_s}))
     x_select << create_text_node('element', 'all')
 
-    x_query << create_text_node('hql', %{select hc from HDeControl hc where #{create_where(tag, term)}#{create_filter(filter)} and (usagetype = 'ARCHIVE' or ((entitytype = 'COMPLEX' or entitytype = 'METS') and usagetype = 'VIEW'))})
+    query_text = [] << 'select' << 'hc' << 'from' << 'HDeControl' << 'hc'
+    where_parts = ["(usagetype='ARCHIVE' or ((entitytype='COMPLEX' or entitytype='METS') and usagetype='VIEW'))"]
+    where_parts << "status is null"
+    where_parts << create_filter(options[:filter]) if options[:filter]
+    where_parts << create_where(tag, term, options[:operator]) unless tag.nil? or tag.empty? or term.nil? or term.empty?
+    query_text << 'where' << where_parts.join(' and ')
+    query_text << create_sort(options[:sort])
+    x_query << create_text_node('hql', query_text.join(' '))
 
-#puts query.to_xml
+puts query.to_xml
     query
   end
 
-  def create_where( tag, value )
+  def create_where(tag, value, operator_input = nil)
+    operator = operator_input || '='
     negate = false
-    if tag =~ /!$/
-      negate = true
-      tag = tag[0..-2]
+    case_sensitive = true
+    if (matchdata = /^(.*)(~)$/.match(tag))
+      tag = matchdata[1]
+      case matchdata[2]
+        when '~'
+          case_sensitive = false
+        else
+      end
     end
-    query_text = %{lower(#{tag}) }
-    if value =~ /.*[^!][%_].*/
-      query_text += %{not } if negate
-      query_text += %{like }
-    else
-      query_text += ( negate ? %{<> } : %{= } )
+
+    wildcard = operator_input.nil? && (value =~ /.*[^!][%_].*/)
+
+    query_text = [] << (case_sensitive ? tag : "upper(#{tag})")
+
+    if wildcard
+      operator = 'like'
+      value = "'#{value}'" if not value =~ /^'.*'$/
     end
-    query_text += %{lower('#{value.to_s}')}
-    
+
+    query_text << operator
+    query_text << (case_sensitive ? value : "upper(#{value})")
+    query_text.join ' '
   end
 
-  def create_filter( filter )
-    result = ['']
+  def create_filter(filter)
+    result = []
     if filter.is_a? Hash
-      filter.each { |tag, value| result << create_where( tag, value) }
+      filter.each { |tag, value| result << create_where(tag, value) }
     end
     result.join ' and '
+  end
+
+  def create_sort(sort = [])
+    return '' if sort.nil? or sort.empty?
+    'order by ' << sort.join(', ')
   end
 
 end
