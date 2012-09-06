8a5608c601f3087fcf806a8f25030b6c0b658fc8
Kris Dekeyser <kris.dekeyser@libis.be>
2012-09-06 19:16:28 +0200

    FIX-FIX: undo change to deployment file. Closes #3.

--- config/deploy.rb
+++ config/deploy.rb
@@ -37,6 +37,7 @@ namespace :remote do
   task :create_symlinks do
     run "ln -ns #{shared_path}/log #{current_path}/"
     run "ln -ns #{shared_path}/pid #{current_path}/"
+    run "ln -ns #{shared_path}/lias_resolver.yml #{current_path}/"
   end
 
   task :stop_server do
