b9139aac0019955d3de9cd2fa7e0b5ae69edbbe7
Kris Dekeyser <kris.dekeyser@libis.be>
2012-09-21 13:03:58 +0200

    FIX: use function parameters for db credentials

--- lib/oracle_connection_pool.rb
+++ lib/oracle_connection_pool.rb
@@ -8,7 +8,7 @@ class OracleConnectionPool < Singleton
 
   def get_connection(db_user, db_pass, db_host)
     if @connection_pool.nil?
-      @connection_pool = OCI8::ConnectionPool.new(0, 30, 1, settings.db_user, settings.db_pass, settings.db_host)
+      @connection_pool = OCI8::ConnectionPool.new(0, 30, 1, db_user, db_pass, db_host)
       @connection_pool.nowait = false
       @connection_pool.timeout = 1
     else
@@ -16,7 +16,7 @@ class OracleConnectionPool < Singleton
       puts "connections: Min: #{@connection_pool.min} Max: #{@connection_pool.max} Open: #{@connection_pool.open_count} Busy:#{@connection_pool.busy_count}"
     end
 
-    OCI8.new(settings.db_user, settings.db_pass, @connection_pool)
+    OCI8.new(db_user, db_pass, @connection_pool)
   end
 
 end
